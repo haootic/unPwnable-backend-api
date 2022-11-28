f7354c967d98df364be6bb42cc3606543d3416db
charlie <haotic@protonmail.com>
2022-11-28 11:59:55 +0000

    FUCK MONGOdb

--- api/app.js
+++ api/app.js
@@ -1,7 +1,16 @@
 const express = require('express');
+var http = require('http');
+
+let port = 8080
+
 const indexRouter = require('./routes/index');
 
 const app = express();
 app.use(express.urlencoded({ extended: false }));
-
 app.use('/', indexRouter);
+app.set('port', port);
+
+var server = http.createServer(app);
+server.listen(port);
+
+console.log("listening to port " + port)
