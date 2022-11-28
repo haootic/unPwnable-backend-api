fe764a7ed5ca48127f34d09079824cfa213eddd8
charlie <haotic@protonmail.com>
2022-11-28 11:25:15 +0000

    define express variable

--- api/app.js
+++ api/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const indexRouter = require('./routes/index');
 
+const app = express();
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/', indexRouter);
